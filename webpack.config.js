6c814809c9c83fe86eca0b89628b3c6d314a7832
Matt McNaney <mcnaneym@appstate.edu>
2019-07-18 12:25:05 -0400

    Added source map to development compile.

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,37 @@
 /* global module, __dirname */
 const path = require('path')
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'index.js',
-    libraryTarget: 'commonjs2',
-  },
-  resolve: {
-    alias: {
-      'react': path.resolve(__dirname, './node_modules/react'),
-      'react-dom': path.resolve(__dirname, './node_modules/react-dom')
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?/,
-        include: path.resolve(__dirname, 'src'),
-        loader: 'babel-loader',
-        query: {
-          presets: ['@babel/preset-env', '@babel/preset-react',]
+module.exports = (env, argv) => {
+  const inProduction = argv.mode === 'production'
+  const inDevelopment = argv.mode === 'development'
+
+  const settings = {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'index.js',
+      libraryTarget: 'commonjs2'
+    },
+    resolve: {
+      alias: {
+        react: path.resolve(__dirname, './node_modules/react'),
+        'react-dom': path.resolve(__dirname, './node_modules/react-dom')
+      }
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?/,
+          include: path.resolve(__dirname, 'src'),
+          loader: 'babel-loader',
+          query: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
         }
-      },
-    ]
-  },
+      ]
+    }
+  }
+  if (inDevelopment) {
+    settings.devtool = 'inline-source-map'
+  }
+  return settings
 }
